33391648ac4f6bdd6be4228b923f8d52ac4c74e7
zhaoshuyu.aki <zhaoshuyu.aki@bytedance.com>
2022-01-12 22:22:16 +0800

    :sparkles: add GithubHeatMapCard

---
+++ src/components/GithubHeatMapBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="github_box">
+    <img class="github_stats" src="https://github-readme-stats.vercel.app/api?username=gnomeek&hide_title=True&show_icons=True&include_all_commits=True&theme=gruvbox_light&hide_border=True">
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.github_box {
+  width: 360px;
+  height: 169px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  border-bottom-left-radius: 14px;
+  border-bottom-right-radius: 14px;
+  background: rgba(251, 241, 199, 1);
+  margin-bottom: 20px;
+  margin-top: 150px
+}
+.github_stats {
+  width: 360px;
+  height: 169px;
+  border: none;
+  /* width: 100%; */
+  /* height: 100%; */
+  /* object-fit:cover; */
+}
+</style>
