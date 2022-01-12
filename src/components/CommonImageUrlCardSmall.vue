46f84e78d26a43d738d50e8018db5105eabd1df1
gnomeek@outlook.com <gnomeek@outlook.com>
2022-01-13 00:29:51 +0800

    :sparkles: add CommonImgAndUrlCardSmall

---
+++ src/components/CommonImageUrlCardSmall.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="commonImgRoot">
+      <a :href="url">
+          <img class="commonImg" :src="img" >
+      </a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CommonImageUrlCardSmall',
+  props: {
+    img: String,
+    url: String
+  }
+}
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.commonImgRoot {
+  width: 169px;
+  height: 169px;
+  border-top-left-radius: 14px;
+  border-top-right-radius: 14px;
+  border-bottom-left-radius: 14px;
+  border-bottom-right-radius: 14px;
+  background: rgb(0, 0, 0);
+  overflow: hidden;
+}
+.commonImg {
+  width: 169px;
+  height: 169px;
+  border: none;
+  /* width: 100%; */
+  /* height: 100%; */
+  
+}
+</style>
