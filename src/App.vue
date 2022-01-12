d9ab75516b93f384e0cf2cb379fcdfa40930b1d1
gnomeek@outlook.com <gnomeek@outlook.com>
2022-01-13 00:32:07 +0800

    :sparkles: add new components and update webfont

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
 <div id="app">
-    <h1> {{data_id}}</h1>
-    <b-container>
+    <!-- <b-container> -->
       <b-row class="justify-content-sm-center">
         <b-col sm="auto">
           <GithubHeatMapBox msg="Welcome to Your Vue.js App"/>
@@ -16,7 +15,16 @@
           <StravaCardSmall :stravaStats="stravaStats" />
         </b-col>
       </b-row>
-    </b-container>
+
+      <b-row class="justify-content-sm-center">
+        <b-col id="left_widget" sm="auto">
+          <CommonImageUrlCardSmall img="https://is3-ssl.mzstatic.com/image/thumb/Purple126/v4/ca/2a/15/ca2a15d4-a84d-7cd6-d7f4-51659f75aaf2/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/460x0w.webp" url="https://app.aves.art/page/app_download" />
+        </b-col>
+        <b-col id="right_widget" sm="auto">
+          <CommonImageUrlCardSmall img="https://raw.githubusercontent.com/CyrusF/AnoMe/main/static/images/favicon.jpg" url="https://github.com/CyrusF/AnoMe" />
+        </b-col>
+      </b-row>
+    <!-- </b-container> -->
 </div>
 </template>
 
@@ -26,6 +34,7 @@ import axios from 'axios';
 import GithubHeatMapBox from './components/GithubHeatMapBox.vue'
 import DoubanCardSmall from './components/DoubanCardSmall.vue';
 import StravaCardSmall from './components/StravaCardSmall.vue';
+import CommonImageUrlCardSmall from './components/CommonImageUrlCardSmall.vue'
 
 export default {
   name: 'App',
@@ -33,6 +42,7 @@ export default {
     GithubHeatMapBox,
     DoubanCardSmall,
     StravaCardSmall,
+    CommonImageUrlCardSmall
   },
   data() {
     return {
@@ -73,23 +83,34 @@ export default {
 @font-face {
   font-family: 'PuhuiTi-Light';
   font-display: swap;
-  src: url('//at.alicdn.com/t/webfont_33zizlczqae.eot'); /* IE9*/
-  src: url('//at.alicdn.com/t/webfont_33zizlczqae.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-  url('//at.alicdn.com/t/webfont_33zizlczqae.woff2') format('woff2'),
-  url('//at.alicdn.com/t/webfont_33zizlczqae.woff') format('woff'), /* chrome、firefox */
-  url('//at.alicdn.com/t/webfont_33zizlczqae.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
-  url('//at.alicdn.com/t/webfont_33zizlczqae.svg#AlibabaPuHuiTiL') format('svg'); /* iOS 4.1- */
+  src: url('//at.alicdn.com/t/webfont_zmpm5ncy4zp.eot'); /* IE9*/
+  src: url('//at.alicdn.com/t/webfont_zmpm5ncy4zp.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+  url('//at.alicdn.com/t/webfont_zmpm5ncy4zp.woff2') format('woff2'),
+  url('//at.alicdn.com/t/webfont_zmpm5ncy4zp.woff') format('woff'), /* chrome、firefox */
+  url('//at.alicdn.com/t/webfont_zmpm5ncy4zp.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
+  url('//at.alicdn.com/t/webfont_zmpm5ncy4zp.svg#AlibabaPuHuiTiL') format('svg'); /* iOS 4.1- */
 }
 
 @font-face {
   font-family: 'PuhuiTi-Bold';
   font-display: swap;
-  src: url('//at.alicdn.com/t/webfont_rdlcu0z2i4b.eot'); /* IE9*/
-  src: url('//at.alicdn.com/t/webfont_rdlcu0z2i4b.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
-  url('//at.alicdn.com/t/webfont_rdlcu0z2i4b.woff2') format('woff2'),
-  url('//at.alicdn.com/t/webfont_rdlcu0z2i4b.woff') format('woff'), /* chrome、firefox */
-  url('//at.alicdn.com/t/webfont_rdlcu0z2i4b.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
-  url('//at.alicdn.com/t/webfont_rdlcu0z2i4b.svg#Alibaba-PuHuiTi-Bold') format('svg'); /* iOS 4.1- */
+  src: url('//at.alicdn.com/t/webfont_1gyjqnctxxn.eot'); /* IE9*/
+  src: url('//at.alicdn.com/t/webfont_1gyjqnctxxn.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+  url('//at.alicdn.com/t/webfont_1gyjqnctxxn.woff2') format('woff2'),
+  url('//at.alicdn.com/t/webfont_1gyjqnctxxn.woff') format('woff'), /* chrome、firefox */
+  url('//at.alicdn.com/t/webfont_1gyjqnctxxn.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
+  url('//at.alicdn.com/t/webfont_1gyjqnctxxn.svg#Alibaba-PuHuiTi-Bold') format('svg'); /* iOS 4.1- */
+}
+
+@font-face {
+  font-family: 'PuhuiTi-Regular';
+  font-display: swap;
+  src: url('//at.alicdn.com/t/webfont_z4n8zrtq4tl.eot'); /* IE9*/
+  src: url('//at.alicdn.com/t/webfont_z4n8zrtq4tl.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+  url('//at.alicdn.com/t/webfont_z4n8zrtq4tl.woff2') format('woff2'),
+  url('//at.alicdn.com/t/webfont_z4n8zrtq4tl.woff') format('woff'), /* chrome、firefox */
+  url('//at.alicdn.com/t/webfont_z4n8zrtq4tl.ttf') format('truetype'), /* chrome、firefox、opera、Safari, Android, iOS 4.2+*/
+  url('//at.alicdn.com/t/webfont_z4n8zrtq4tl.svg#Alibaba-PuHuiTi-Regular') format('svg'); /* iOS 4.1- */
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -105,9 +126,11 @@ export default {
 #right_widget {
   padding-right: 0px;
   padding-left: 11px;
+  margin-top:20px;
 }
 #left_widget {
   padding-right: 11px;
   padding-left: 0px;
+  margin-top:20px;
 }
 </style>
